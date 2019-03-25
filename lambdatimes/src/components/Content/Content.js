d740c2315409d3e80a576767dc42f4b478bf4c40
Brandi Apetsi <Brandi@Brandis-iMac.local>
2019-03-25 14:35:16 -0400

    fixed Tab props className

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -19,8 +19,8 @@ export default class Content extends Component {
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
     this.setState({
-      tabs: tabData.data,
-      cards: cardData.data
+      tabs: tabData,
+      cards: cardData
     })
   }
 
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,13 +4,20 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
-      props.tab === props.selectedTab ?
-      props.tab.className = 'tab active-tab' :
-      props.tab.className = 'tab'
+
+      
+      // props.tab === props.selectedTab ?
+      // className={tab active-tab} :
+      // className='tab'
   return (
     <div
-      className={''}
-      onClick={(event) => {
+      className={
+        props.tab === props.selectedTab ?
+        'tab active-tab' :
+        'tab'
+      }
+
+      onClick={(tab) => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
          props.selectTabHandler(props.tab)
