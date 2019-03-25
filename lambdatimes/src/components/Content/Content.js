98abc3170d925319e3e7aa2bf76e12e3c8b60183
Brandi Apetsi <Brandi@Brandis-iMac.local>
2019-03-25 01:22:12 -0400

    work in progress

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -46,9 +46,9 @@ export default class Content extends Component {
    
     this.state.selected === 'all' ? 
     this.state.cards :
-    this.state.cards.filter(card => {
+    this.state.cards.filter(card => (
       card.tab === this.state.selected && card
-    })
+    ))
   };
 
   render() {
