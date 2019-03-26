c8fd4cc8fb933cc8d5f3ee1cb0ce5ebb24e452fd
Brandi Apetsi <Brandi@Brandis-iMac.local>
2019-03-25 20:15:49 -0400

    put answers in index.js and answered them

--- lambdatimes/src/index.js
+++ lambdatimes/src/index.js
@@ -4,3 +4,17 @@ import './CSS/index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+// What are PropTypes used for? Please describe why it's important to type check our data in JavaScript.
+//Proptypes are used to prevent mistakes by checking the typeOf of the incoming props
+
+// Describe a life-cycle event in React?
+//mount, update, unmount
+
+// Explain the details of a Higher Order Component?
+//HOCs anadvanced way to designing components that share data or a behavior in a way that is different from a traditional state to props pattern
+
+
+// What are three different ways to style components in React? Explain some of the benefits of each.
+// CSS - 
+// ReactStrap - 
+// BootStrap - 
